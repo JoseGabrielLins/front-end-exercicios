38cf3c425ed0beb99e146147a90efa09f8cf22e0
gbrielbram18 <69555814+JoseGabrielLins@users.noreply.github.com>
2025-09-18 21:38:37 -0300

    for

---
+++ exercicios-aluno/exercicio_laco_5/main.js
@@ -0,0 +1,10 @@
+let= soma = 0;
+
+for(let cont = 0;cont<11;cont=cont+1){
+    let peso = parseFloat(prompt("Digite o peso"));
+    soma +=peso;
+}
+
+let resultado = soma/10;
+
+console.log(resultado)
---
+++ exercicios-aluno/exercicio_laco_6/main.js
@@ -0,0 +1,10 @@
+let acima = 0;
+
+for(let cont = 0;cont<11;cont++){
+    let numero = parseFloat(prompt("Digite um número"));
+
+    if(numero > 50){
+        acima +=1;
+    }
+}
+    console.log(acima)
